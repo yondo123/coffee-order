b153dffb9040a2dce8272cf138a1d407a30e8e6b
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-10-22 21:31:03 +0900

    🔧 modify: early return 형식으로 변경

--- src/productDetail/components/ProductDetail.tsx
+++ src/productDetail/components/ProductDetail.tsx
@@ -9,24 +9,24 @@ import { ImageHalf } from '@layout/mixins';
 export const ProductDetail = () => {
   const productId = useRouterLocation();
   const productDetailInfo = useRequestProductDetail(productId);
+
+  if (isEmptyObject(productDetailInfo)) {
+    return <EmptyPage message="해당하는 상품이 존재하지 않습니다." />;
+  }
+
   return (
-    <>
-      {isEmptyObject(productDetailInfo) && <EmptyPage message="해당하는 상품이 존재하지 않습니다." />}
-      {!isEmptyObject(productDetailInfo) && (
-        <section>
-          <h2>{productDetailInfo.name}</h2>
-          <ProductDetailContainer>
-            <ImageHalf>
-              <img src={productDetailInfo.imageUrl} alt={productDetailInfo.name} />
-            </ImageHalf>
-            <ProductOption
-              productName={productDetailInfo.name}
-              productPrice={productDetailInfo.price}
-              productOptions={productDetailInfo.productOptions}
-            />
-          </ProductDetailContainer>
-        </section>
-      )}
-    </>
+    <section>
+      <h2>{productDetailInfo.name}</h2>
+      <ProductDetailContainer>
+        <ImageHalf>
+          <img src={productDetailInfo.imageUrl} alt={productDetailInfo.name} width={128} height={128} />
+        </ImageHalf>
+        <ProductOption
+          productName={productDetailInfo.name}
+          productPrice={productDetailInfo.price}
+          productOptions={productDetailInfo.productOptions}
+        />
+      </ProductDetailContainer>
+    </section>
   );
 };
