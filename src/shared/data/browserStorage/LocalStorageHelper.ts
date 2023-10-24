bcab80f510a885f9603bc76ea164dca62b57d2a6
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-10-24 09:11:09 +0900

    🐛 fix: localStorage 존재하지 않는 값일 경우, null로 반환하도록 수정

--- src/shared/data/browserStorage/LocalStorageHelper.ts
+++ src/shared/data/browserStorage/LocalStorageHelper.ts
@@ -2,8 +2,8 @@ import type { StorageKeys } from './StorageKeys';
 
 class LocalStorageHelper {
   static get(key: StorageKeys) {
-    const item = localStorage.getItem(key) ?? '';
-    return JSON.parse(item);
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
   }
 
   static set<T>(key: StorageKeys, value: T): void {
