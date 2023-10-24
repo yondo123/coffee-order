afacf53a55fa878148c9967496c4d2d65552da59
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-10-24 20:29:30 +0900

    🔧 modify: button type 추가, onClick 옵셔널 속성으로 변경

--- src/layout/components/Button.tsx
+++ src/layout/components/Button.tsx
@@ -2,13 +2,13 @@ import { ButtonStyle } from '../styles/buttonStyle';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
-  onClick: () => void;
   disabled?: boolean;
+  onClick?: () => void;
 }
 
 export const Button = ({ children, onClick, disabled = false }: ButtonProps) => {
   return (
-    <ButtonStyle onClick={onClick} disabled={disabled}>
+    <ButtonStyle type="button" onClick={onClick ? onClick : undefined} disabled={disabled}>
       {children}
     </ButtonStyle>
   );
