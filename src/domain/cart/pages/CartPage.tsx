8fff838b8c9dd3c0b7333238db7229955f32623e
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-10-31 17:32:39 +0900

    ✨ feat: Suspense에 Loader 컴포넌트 적용

--- src/domain/cart/pages/CartPage.tsx
+++ src/domain/cart/pages/CartPage.tsx
@@ -1,12 +1,12 @@
 import { Suspense } from 'react';
-import { Heading } from '@layout/components';
+import { Heading, Loader } from '@layout/components';
 import { PageContainer } from '@layout/styles/pageContainerStyle';
 import { CartProvider, CartList, TotalPrice, CartOrder } from '../components';
 
 export const CartPage = () => {
   return (
     <CartProvider>
-      <Suspense>
+      <Suspense fallback={<Loader />}>
         <PageContainer>
           <Heading size="xxl">장바구니</Heading>
           <CartList />
