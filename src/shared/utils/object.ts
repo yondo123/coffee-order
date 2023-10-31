a2e6b516c26efd41d4904a052b9757ee6f2bcb55
yondo123 <46988995+yondo123@users.noreply.github.com>
2023-10-31 19:20:24 +0900

    🔧 modify: 배열 리스트 빈 값 체크 추가

--- src/shared/utils/object.ts
+++ src/shared/utils/object.ts
@@ -1,5 +1,9 @@
-export const isEmptyObject = (obj: object | undefined): boolean => {
-  if (!obj) {
+export const isEmptyObject = (obj: object | undefined | unknown[]): boolean => {
+  if (!obj || typeof obj !== 'object') {
+    return true;
+  }
+
+  if (Array.isArray(obj) && !obj.length) {
     return true;
   }
   return Object.keys(obj).length === 0 && obj.constructor === Object;
